2d065a40e293bf2c934b340cf093f4fdac5b5597
KamaevAnton <kamant.otr.rarus@gmail.com>
2025-09-16 10:30:25 +0300

    Фикс. Убрал форматирование для php в nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Отключаем форматирование при сохранении для PHP
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "php",
+  callback = function()
+    vim.b.autoformat = false
+  end,
+})
